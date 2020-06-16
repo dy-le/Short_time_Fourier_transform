79a060b52e87553efa2c976568dcbc302c238334
daemonLee <ldduy.edu99@gmail.com>
2020-06-16 23:34:34 +0700

    add analyse wav

--- STFT_Matlab/exmaple.m
+++ STFT_Matlab/exmaple.m
@@ -1,10 +1,16 @@
 clear, clc, close all
 % load an audio file
-%[x, fs] = audioread('PinkPanther60.wav');   % load an audio file
-%x = x(:, 1);                        % get the first channel
-fs = 10e3; % Sample rate
+[x, fs] = audioread('PinkPanther60.wav');   % load an audio file
+x = x(:, 1);                        % get the first channel
+% fs = 10e3; % Sample rate
 t = 0:1/fs:2; % Sample time
-x = vco(sin(2*pi*t),[0.1 0.4]*fs,fs);
+% x = vco(sin(2*pi*t),[0.1 0.4]*fs,fs);
+figure(1)
+plot(x);
+xlabel('Time, s')
+ylabel('Amplitude')
+title('Original Signal');
+
 % define analysis parameters
 wlen = 1024;                        % window length (recomended to be power of 2)
 hop = wlen/4;                       % hop size (recomended to be power of 2)
@@ -12,9 +18,9 @@ nfft = 4096;                        % number of fft points (recomended to be pow
 % perform STFT
 win = blackman(wlen, 'periodic'); % hamming
 [S, f, t] = stft_edit(x, win, hop, nfft, fs);
-% s — Short-time Fourier transform
-% f — Frequencies
-% t — Time instants
+% s - Short-time Fourier transform
+% f - Frequencies
+% t - Time instants
 % calculate the coherent amplification of the window
 C = sum(win)/wlen;
 % take the amplitude of fft(x) and scale it, so not to be a
@@ -29,11 +35,21 @@ end
 % convert amplitude spectrum to dB (min = -120 dB)
 S = 20*log10(S + 1e-6);
 % plot the spectrogram
-figure(1)
+figure(2)
 surf(t, f, S)
 shading interp
 axis tight
-view(-45, 65)
+view(65, -45)
+set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
+xlabel('Time, s')
+ylabel('Frequency, Hz')
+title('Amplitude spectrogram of the signal')
+
+figure(3)
+surf(t, f, S)
+shading interp
+axis tight
+view(90, 90)
 set(gca, 'FontName', 'Times New Roman', 'FontSize', 14)
 xlabel('Time, s')
 ylabel('Frequency, Hz')
